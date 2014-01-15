615e3b6c119e702222c4d5d5a44594f8abe5a3e5
taterbase <shankga@gmail.com>
2014-01-14 17:22:11 -0700

    Add ipv6 and <sleep>

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ module.exports = function mongotop (opts) {
     opts.port = '27017'
 
   var args = []
+    , sleep = 1
     , locks = false
 
   Object.keys(opts).forEach(function(key) {
-    if (key === 'locks') {
-      locks = opts[key]
-      return args = args.concat('--locks')
+    switch (key) {
+      case 'locks':
+        if (opts['locks'])
+          args = args.concat('--locks')
+        locks = opts[key]
+        break
+      case 'ipv6':
+        if (opts['ipv6'])
+          args = args.concat('--ipv6')
+        break
+      case 'sleep':
+        sleep = opts[key]
+        break
+      default:
+        args = args.concat(['--' + key, '' + opts[key]])
     }
-
-    args = args.concat(['--' + key, '' + opts[key]])
   })
+
+  args.push(sleep)
 
   var mstream = new stream({locks: locks})
 
